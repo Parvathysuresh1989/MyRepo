3ce97f8bd06cabf27c725549b93f36767c6f2853
user <user@192.168.1.35>
2024-12-29 15:41:38 +0530

    access commit

---
+++ FirstMavenProject/src/main/java/accessspecifierpackage/AccesSpecierClass.java
@@ -0,0 +1,13 @@
+package accessspecifierpackage;
+
+import accessspecifier.AccessSpecifierEg;
+
+public class AccesSpecierClass
+{
+	public static void main(String args[])
+	{
+		AccessSpecifierEg obj2=new AccessSpecifierEg();
+		obj2.add();
+		
+	}
+}
